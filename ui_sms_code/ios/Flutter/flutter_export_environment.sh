71780e370e5fe3908cb11e29b14c3d0c5b9ba63a
Renato A dos Santos <40146884+Git-RenatoAlcantara@users.noreply.github.com>
2020-05-09 21:11:43 -0300

    Ui Validate Code
    Inputs com formato de código recebido por sms para confirmação de numero.

---
+++ ui_sms_code/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/renato/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/renato/development/ui_validate_code/ui_validate_code"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/renato/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
